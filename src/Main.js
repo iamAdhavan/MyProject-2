de0b48dcd27fc75a82727861debf50b441c2ca22
Adhavan <163775984+iamAdhavan@users.noreply.github.com>
2025-01-26 11:38:51 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -50,7 +50,7 @@ function Main() {
 
   
       <div className="hero-section">
-      <div className="navbar">
+      <div className="navbar-main">
       
       <img src="https://images.ctfassets.net/y2ske730sjqp/821Wg4N9hJD8vs5FBcCGg/9eaf66123397cc61be14e40174123c40/Vector__3_.svg?w=460" alt="Netflix Logo" className="navbar-logo" />
       
